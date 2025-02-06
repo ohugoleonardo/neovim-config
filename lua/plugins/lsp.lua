11dfff0894b5364e40624830c2dda15d540ff01e
Hugo Leonardo <ohugoleonardo1@gmail.com>
2025-02-06 18:37:04 -0300

    add mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,18 @@
 return {
     'neovim/nvim-lspconfig',
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
     config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            -- ensure_installed = { "jdtls" },
+        })
+
         local lspconfig = require("lspconfig")
-            lspconfig.pyright.setup({})    -- LSP para Python
-            lspconfig.clangd.setup({})     -- LSP para C/C++
-            lspconfig.ts_ls.setup({})      -- LSP para JavaScript/TypeScript
+            lspconfig.pyright.setup({})                   -- LSP para Python
+            lspconfig.clangd.setup({})                    -- LSP para C/C++
+            lspconfig.ts_ls.setup({})                     -- LSP para JavaScript/TypeScript
         end,
 }
