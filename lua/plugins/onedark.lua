55484b77669bb3288eb8103067dbba2c923496c1
Hugo Leonardo <ohugoleonardo1@gmail.com>
2025-02-15 17:03:20 -0300

    fix colorscheme

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -4,6 +4,15 @@ return {
         require('onedark').setup {
             style = "darker",
         }
+    code_style = {
+        comments = 'none',
+        keywords = 'none',
+        functions = 'none',
+        strings = 'none',
+        variables = 'none'
+    },
+
+
     require('onedark').load()
     end,
 }
